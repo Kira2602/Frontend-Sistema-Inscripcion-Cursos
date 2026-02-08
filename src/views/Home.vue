ffe34df1cd91028165413dcc19605c1d31cdac73
Rosario Calisaya <misa26amane@gmail.com>
2026-02-08 11:37:16 -0400

    Inicializando entorno de desarrollo

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>Proyecto Sistema de Inscripción de Cursos</p>
+    </div>
+</template>
+
+<script>    
+</script>
+
+<style scoped>  
+</style>
