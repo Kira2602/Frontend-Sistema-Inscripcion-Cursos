93aa77b8280c74e8260de7423f0c45365cb087ea
AdrianOrdonez2705 <fabriadri2705@gmail.com>
2026-03-01 16:25:51 -0400

    (func) miCarrera: Desplegar carreras disponibles

--- src/modulos/estudiantes/views/miCarrera.vue
+++ src/modulos/estudiantes/views/miCarrera.vue
@@ -3,7 +3,38 @@
 
   <div v-else>
     <div v-if="student && student.data.carrera_usuario === null">
-      <h1>Elegir carrera</h1>
+      <h1 class="title">Carreras Disponibles</h1>
+      <p class="description">
+        Aún no estás inscrito en ninguna carrera. Este es el momento perfecto
+        para dar el primer paso hacia tu futuro profesional. Explora nuestras
+        carreras disponibles, descubre nuevas oportunidades y elige el camino
+        que mejor se adapte a tus metas y aspiraciones. Empieza hoy a construir
+        tu futuro académico con confianza, compromiso y el respaldo que
+        necesitas para alcanzar el éxito.
+      </p>
+      <div class="carrera-grid">
+        <div class="carrera-card" v-for="carrera in carreras.data">
+          <h2>{{ carrera.nombre }}</h2>
+          <p class="desc">{{ carrera.descripcion }}</p>
+          <div class="specs">
+            <p class="spec">
+              <SimpleIcon iconName="grid" iconColor="" />
+              Código: {{ String(carrera.codigo).toUpperCase() }}
+            </p>
+            <p class="spec">
+              <SimpleIcon iconName="verified" iconColor="" />
+              Certificación
+            </p>
+            <p class="spec">
+              <SimpleIcon iconName="info" iconColor="" />
+              Duración: {{ carrera.duracion }}
+            </p>
+          </div>
+          <div class="button-group">
+            <button class="insc">Inscribirse</button>
+          </div>
+        </div>
+      </div>
     </div>
 
     <div v-else-if="student">
@@ -32,30 +63,18 @@
               <SimpleIcon iconName="datos" iconColor="orange" />
               Mis Datos:
             </h3>
-            <p>
-                <p class="titu">Nombre:</p> 
-                {{ student.data.nombre }}
-            </p>
-            <p>
-                <p class="titu">CI:</p> 
-                {{ student.data.ci }}
-            </p>
-            <p>
-                <p class="titu">Correo:</p> 
-                {{ student.data.correo }}
-            </p>
-            <p>
-                <p class="titu">Fecha de Nacimiento:</p> 
-                {{ student.data.fecha_nac }}
-            </p>
-            <p>
-                <p class="titu">Dirección:</p> 
-                {{ student.data.direccion }}
-            </p>
-            <p>
-                <p class="titu">Teléfono:</p> 
-                {{ student.data.telefono }}
-            </p>
+            <p class="titu">Nombre:</p>
+            {{ student.data.nombre }}
+            <p class="titu">CI:</p>
+            {{ student.data.ci }}
+            <p class="titu">Correo:</p>
+            {{ student.data.correo }}
+            <p class="titu">Fecha de Nacimiento:</p>
+            {{ student.data.fecha_nac }}
+            <p class="titu">Dirección:</p>
+            {{ student.data.direccion }}
+            <p class="titu">Teléfono:</p>
+            {{ student.data.telefono }}
           </div>
         </div>
       </div>
@@ -74,6 +93,7 @@ import SimpleIcon from "../../seguridad/components/SimpleIcon.vue";
 
 const student = ref(null);
 const loading = ref(true);
+const carreras = ref([]);
 
 onMounted(async () => {
   try {
@@ -88,6 +108,14 @@ onMounted(async () => {
       const data = await response.data;
       student.value = data;
     }
+
+    const carrerasResponse = await api.get("/carreras/disponibles", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const carreraData = await carrerasResponse.data;
+    carreras.value = carreraData;
   } catch (error) {
     console.error("Error al obtener el usuario:", error);
   } finally {
@@ -182,7 +210,84 @@ onMounted(async () => {
 }
 
 .titu {
-    margin-top: 1rem;
-    font-weight: bold;
+  margin-top: 1rem;
+  font-weight: bold;
+}
+
+.carrera-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+  padding: 20px;
+}
+
+.carrera-card {
+  background-color: white;
+  border: 1px solid #dcdcdc;
+  border-radius: 12px;
+  padding: 0.6rem 1rem;
+  box-shadow: 10px 10px 12px 5px #dcdcdc;
+}
+
+.desc {
+  margin: 0.5rem 0rem;
+  color: #787878;
+  font-size: small;
+}
+
+.specs {
+  display: flex;
+  gap: 5px;
+  margin: 1rem 0rem;
+}
+
+.spec {
+  cursor: default;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  padding: 0.5rem 0.8rem;
+  font-size: small;
+  gap: 5px;
+}
+
+.spec:nth-child(1) {
+  background-color: #680092;
+  color: #de8bff;
+}
+
+.spec:nth-child(2) {
+  background-color: #005414;
+  color: #00ff3c;
+}
+
+.spec:nth-child(3) {
+  background-color: rgb(150, 120, 0);
+  color: rgb(255, 255, 0);
+}
+
+.button-group {
+  margin: 2rem 0rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.insc {
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  background-color: #003cff;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  padding: 0.6rem 2rem;
+  font-weight: 600;
+}
+
+.insc:hover {
+  text-decoration: underline;
 }
 </style>
