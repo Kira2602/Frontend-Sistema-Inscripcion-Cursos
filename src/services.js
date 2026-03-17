f7a4bfaed55bd4d2434d57284e5a4d1086c46b67
Rosario Calisaya Calderon <125493619+Kira2602@users.noreply.github.com>
2026-03-17 18:26:13 -0400

    Switch to dynamic baseURL for API

--- src/services.js
+++ src/services.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    "Content-Type": "application/json",
-  },
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
